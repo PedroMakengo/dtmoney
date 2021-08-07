4d1040dea0de3d28a5ef45e729940bc9d31c9f60
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-08-07 19:00:11 -0300

    Terminando o Header da minha aplicação

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -30,7 +30,7 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   body {
-    background: var(---background);
+    background: var(--background);
     -webkit-font-smoothing: antialiased;
   }
 
