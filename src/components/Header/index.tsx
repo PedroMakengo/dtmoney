739bbfddd4654b1bb0729f472e6c8ec38a31e5b8
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-08-08 09:33:30 -0300

    Adicionando modal-dentro-header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,20 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
 import logoImg  from '../../assets/logo.svg';
 
 import { Container, Content } from './styles';
 
 export function Header() {
+  const [isNewTransictionModalOpen, setIsNewTransictionModalOpen] = useState(false);
+
+  function handleOpenNewTransictionModal() {
+    setIsNewTransictionModalOpen(true);
+  }
+
+  function handleCloseNewTransictionModal() {
+    setIsNewTransictionModalOpen(false);
+  }
+
   return (
     <Container>
       <Content>
