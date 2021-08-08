30a06e167ed47a926f55a066dde2217cdd0972e3
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-08-08 05:05:50 -0300

    Create my TransictionTable

---
+++ src/components/TransictionsTable/index.tsx
@@ -0,0 +1,14 @@
+import {Container} from './styles';
+
+export function TransictionsTable() {
+  return (
+    <table>
+      <thead>
+        <th></th>
+      </thead>
+      <tbody>
+        <td></td>
+      </tbody>
+    </table>
+  )
+}
