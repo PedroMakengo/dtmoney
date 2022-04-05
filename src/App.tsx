d5ff710e6c52553506cd909cb37b9a98dd26d9b8
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2022-04-05 03:58:09 -0300

    Trabalhando com o styled-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: 64px;
+  color: #8257e6;
+
+  button {
+    background: #000;
+  }
+`;
+
 export function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Title>
+        Hello World
+        <button>Salvar</button>
+      </Title>
     </div>
   );
 }
