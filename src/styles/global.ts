1c443c02438ebb2236afe10cc27338be44ba6cf4
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2022-04-05 04:58:25 -0300

    Terminando a configuração do meu arquivo global

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -13,7 +13,7 @@ export const GlobalStyle = createGlobalStyle`
     
     --shape: #FFFFFF;
   }
-  
+
   * {
     margin: 0;
     padding: 0;
@@ -33,6 +33,15 @@ export const GlobalStyle = createGlobalStyle`
   body {
     background:var(--background);
     -webkit-font-smoothing: antialiased
+  }
+
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5,h6, strong {
+    font-weight: 600
   }
 
   button {
