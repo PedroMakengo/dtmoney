41e3e1d188669336c0e6222317858ad19f50cdd7
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2022-04-06 14:49:26 -0300

    Terminando o meu módulo de componentização

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Container } from "./styles";
 import { Sumary } from "../Sumary";
+import { TransactionsTable } from "../TransactionsTable";
 
 export function Dashboard() {
   return (
     <Container>
       <Sumary />
+      <TransactionsTable />
     </Container>
   );
 }
