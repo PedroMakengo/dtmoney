709db8d301169a2be3881bdac0972b4a0cd502a8
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2022-04-07 18:18:04 -0300

    Fazendo a instalação do miragejs e axios

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,12 @@
+import { useEffect } from "react";
 import { Container } from "./styles";
 
 export function TransactionsTable() {
+  useEffect(() => {
+    fetch("https://localhost:3000/api/transactions")
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  }, []);
   return (
     <Container>
       <table>
