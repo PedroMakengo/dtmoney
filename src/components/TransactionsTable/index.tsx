87cd5a2fdebcdc45d8f2abba0af7d623e5286368
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2022-04-07 18:40:33 -0300

    Adicionando o miragejs e o axios

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,11 +1,10 @@
 import { useEffect } from "react";
+import { api } from "../../services/api";
 import { Container } from "./styles";
 
 export function TransactionsTable() {
   useEffect(() => {
-    fetch("https://localhost:3000/api/transactions")
-      .then((response) => response.json())
-      .then((json) => console.log(json));
+    api.get("/transactions").then((data) => console.log(data));
   }, []);
   return (
     <Container>
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createServer } from "miragejs";
 import { App } from "./App";
+
+// Criando o meu servidor
+createServer({
+  // Criando a minha rota
+  routes() {
+    // Criando o namespace
+    this.namespace = "api";
+
+    // Criando
+    this.get("/transactions", () => {
+      return [
+        {
+          id: 1,
+          title: "Transaction 1",
+          amount: 400,
+          type: "deposit",
+          category: "Food",
+          cratedAt: new Date(),
+        },
+      ];
+    });
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
