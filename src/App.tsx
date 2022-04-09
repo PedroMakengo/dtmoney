165c02cf5b15cb02dcbe3a57f7b07714161d3fe6
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2022-04-09 19:19:37 -0300

    Aprendendo sobre como passar propriedades para outros componentes

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Modal from "react-modal";
 
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 import { GlobalStyle } from "./styles/global";
 
 Modal.setAppElement("#root");
@@ -25,12 +26,11 @@ export function App() {
 
       <Dashboard />
 
-      <Modal
+      <NewTransactionModal
         isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Cadastrar transação</h2>
-      </Modal>
+      />
+
       <GlobalStyle />
     </>
   );
